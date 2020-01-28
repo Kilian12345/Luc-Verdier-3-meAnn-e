67f9214b6ae815023dea16ca2dcb4a85abcccb45
Kilian <allairekilian@gmail.com>
2020-01-28 16:28:05 +0100

    Grille + Snap 100%

---
+++ SemaineIntensiveProto/Assets/Scripts/KIKI-Scripts/DragAndDrop.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+
+    Material mat;
+    private Grid grid;
+    RaycastHit hitInfo;
+    Ray ray;
+
+    private void Start()
+    {
+        grid = FindObjectOfType<Grid>();
+        mat = GetComponent<Material>();
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+
+        PlaceCubeNear(ray.GetPoint(distance));
+        
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = new Vector3(rayPoint.x - 0.5f, rayPoint.y - 0.5f, 0);
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
