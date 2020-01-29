289a73732bd1567e0aa7197e6ba0739517e5b81d
Maxence <maxdurlon@gmail.com>
2020-01-29 14:49:34 +0100

    Sourcetree c'est zucced un peu

---
+++ SemaineIntensiveProto/Assets/Scripts/Max-Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider
+{
+    public int amount;
+    public GameObject sliderObject;
+}
+
+public class Level
+{
+    public int tiles;
+    public List<Slider> sliders;
+
+    //public int entryTileRef
+    //public int exitTileRef
+}
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Level Generator")]
+    public List<Level> levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateLevel(int nTile)
+    {
+
+    }
+}
